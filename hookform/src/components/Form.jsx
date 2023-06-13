62e75faf88dc1eb35ff81e3feed524f4a58f500b
Jaime Rodriguez <129904062+jrxdriguez@users.noreply.github.com>
2023-06-12 23:28:55 -0700

    Add files via upload

---
+++ hookform/src/components/Form.jsx
@@ -0,0 +1,69 @@
+import React, {useState} from 'react'
+
+const Form = () => {
+    // State variables: first name, last name, email, password, confirm password
+
+    const [first, setFirst] = useState("")
+    const [last, setLast] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [confirm, setConfirm] = useState("")
+
+    let [listOfUsers, setListOfUsers] = useState([])
+
+    const createUser = (e) => {
+        e.preventDefault()
+        let userObject = {first, last, email, password, confirm}
+        setListOfUsers([...listOfUsers, userObject])
+    }
+
+  return (
+    <div>
+        <br></br>
+        <form onSubmit={ createUser } className = "form" >
+            <div>
+                <label>First Name: </label> 
+                <input type="text" onChange={ (e) => setFirst(e.target.value) } />
+            </div><br></br>
+            <div>
+                <label>Last Name: </label> 
+                <input type="text" onChange={ (e) => setLast(e.target.value) } />
+            </div><br></br>
+            <div>
+                <label>Email: </label> 
+                <input type="text" onChange={ (e) => setEmail(e.target.value) } />
+            </div><br></br>
+            <div>
+                <label>Password: </label>
+                <input type="password" onChange={ (e) => setPassword(e.target.value) } />
+            </div><br></br>
+            <div>
+                <label> Confirm Password: </label>
+                <input type="password" onChange={ (e) => setConfirm(e.target.value) } />
+            </div><br></br>
+            <input type="submit" value="Create User" className = "btn btn-outline-dark mt-3" />
+        </form><hr></hr>
+
+        <div className = "form2">
+            <h3>Your Form Data</h3><br></br>
+            {
+                listOfUsers.map((user, index) => {
+                    return(
+                        <div key = {index}>
+                            <h5>First Name: {user.first}</h5>
+                            <h5>Last Name: {user.last}</h5>
+                            <h5>Email: {user.email}</h5>
+                            <h5>Password: {user.password}</h5>
+                            <h5>Confirm Password: {user.confirm}</h5>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    </div>
+  )
+}
+
+export default Form
+
+// form: onSubmit={ createUser }
